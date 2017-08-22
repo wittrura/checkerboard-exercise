24a20597a6f8bc40c26ff83e61dcf7429c5356e0
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-21 20:47:08 -0500

    Adds js to build board with coloring

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JS goes here
+console.log('js file connected');
+
+for (var i = 0; i < 81; i++) {
+  let newDiv = document.createElement('div');
+  newDiv.setAttribute('style', "width: 11.1%; float: left; padding-bottom: 11.1%;");
+  if (i % 2 === 0) {
+    newDiv.style.backgroundColor = "black";
+  } else {
+    newDiv.style.backgroundColor = "red";
+  }
+  document.body.appendChild(newDiv);
+}
