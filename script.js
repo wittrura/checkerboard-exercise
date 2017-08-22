d4a91603f9f995d0379a2fcdf7ac7a05df29254f
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-21 21:59:57 -0500

    Adds feature for updating colors on time intervals

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function populateBoard() {
   }
 }
 
+// updates the entire board at once
 function updateColors () {
   let colorDivs = document.getElementsByTagName('div');
   for (var i = 0; i < colorDivs.length; i++) {
@@ -28,5 +29,16 @@ function updateColors () {
   setTimeout(updateColors, 2000);
 }
 
+// updates a randomly selected div
+function updateColorsSequential() {
+  window.setInterval(function() {
+    let colorDivs = document.getElementsByTagName('div');
+    let randomDiv = colorDivs[Math.floor(Math.random() * colorDivs.length)];
+    randomDiv.style.backgroundColor = generateRandomHexColor();
+  }, 100);
+}
+
+
 populateBoard();
 updateColors();
+updateColorsSequential();
