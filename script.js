6d88f6cfe4599cd9ca77b11fe7e0234211943d71
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-21 21:06:15 -0500

    Adds feature for random colors on board

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-console.log('js file connected');
-
-for (var i = 0; i < 81; i++) {
-  let newDiv = document.createElement('div');
-  newDiv.setAttribute('style', "width: 11.1%; float: left; padding-bottom: 11.1%;");
-  if (i % 2 === 0) {
-    newDiv.style.backgroundColor = "black";
-  } else {
-    newDiv.style.backgroundColor = "red";
-  }
-  document.body.appendChild(newDiv);
+function generateRandomLetterNum() {
+  let str = 'abcdef0123456789'
+  return str[Math.floor(Math.random() * str.length)];
 }
+
+function generateRandomHexColor(){
+  let hexColor = '#'
+  for (var i = 0; i < 6; i++) {
+    hexColor += generateRandomLetterNum();
+  }
+  return hexColor;
+}
+
+function populateBoard() {
+  for (var i = 0; i < 81; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('style', "width: 11.1%; float: left; padding-bottom: 11.1%;");
+    newDiv.style.backgroundColor = generateRandomHexColor();
+    document.body.appendChild(newDiv);
+  }
+}
+
+populateBoard();
+console.log(generateRandomHexColor());
